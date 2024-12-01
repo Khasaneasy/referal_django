91657d27821acfd0cecdcbe60ef3fda21dd7c52f
Khasaneasy <kabardos.noob@gmail.com>
2024-12-01 12:47:24 +0300

    add_Seri

--- referrals/urls.py
+++ referrals/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-
-from .views import RequestPhoneNumberView, ProfileView, RequestAuthCodeView
+from .views import RequestPhoneNumberView, RequestAuthCodeView, ProfileView
 
 urlpatterns = [
-    path('request-phone/', RequestPhoneNumberView.as_view(), name='request-phone'),
-    path('request_auth_code/', RequestAuthCodeView.as_view(), name='request_auth_code'),
-    path('profile/', ProfileView.as_view(), name='user-profile'),
+    path('auth/request-phone/', RequestPhoneNumberView.as_view(), name='request-phone'),
+    path('auth/request-auth-code/', RequestAuthCodeView.as_view(), name='request-auth-code'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
--- referrals/views.py
+++ referrals/views.py
@@ -1,13 +1,15 @@
+import logging
 import random
 import time
 from http import HTTPStatus
 
+from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import User
-import logging
+from .serializers import PhoneNumberSerializer, UserProfileSerializer, AuthCodeSerializer, InviteCodeSerializer
 
 logger = logging.getLogger(__name__)
 
@@ -19,75 +21,56 @@ class ProfileView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        phone_number = request.user.phone_number
-        user = User.objects.filter(phone_number=phone_number).first()
-
-        if not user:
-            return Response({'error': 'Пользователь не найден'},
-                            status=HTTPStatus.NOT_FOUND)
-        profile_data = {
-            'phone_number': user.phone_number,
-            'invite_code':  user.invite_code,
-            'activated_invite_code': user.activated_invite_code or 'Не активирован'
-        }
-        return Response(profile_data, status=HTTPStatus.OK)
+        serializer = UserProfileSerializer(request.user)
+        return Response(serializer.data, status=HTTPStatus.OK)
 
     def post(self, request):
-        phone_number = request.user.phone_number
-        invite_code = request.data.get('invite_code')
+        serializer = InviteCodeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        invite_code = serializer.validated_data['invite_code']
 
-        if not invite_code:
-            return Response({'error': 'Инвайт-код обязателен'},
-                            status=HTTPStatus.BAD_REQUEST)
-
-        user = User.objects.filter(phone_number=phone_number).first()
-        if user.activated_invite_code:
-            return Response({'message': 'Инвайт-код уже активирован'},
-                            status=HTTPStatus.BAD_REQUEST)
+        if request.user.activated_invite_code:
+            return Response({'message': 'Инвайт-код уже активирован'}, status=HTTPStatus.BAD_REQUEST)
 
         inviter = User.objects.filter(invite_code=invite_code).first()
         if not inviter:
             return Response({'error': 'Неверный инвайт-код'}, status=HTTPStatus.BAD_REQUEST)
 
-        user.activated_invite_code = invite_code
-        user.invited_by = inviter
-        user.save()
+        request.user.activated_invite_code = invite_code
+        request.user.invited_by = inviter
+        request.user.save()
 
-        return Response({'message': f'Инвайт-код {invite_code} успешно активирован'},
-                        status=HTTPStatus.OK)
+        return Response({'message': f'Инвайт-код {invite_code} успешно активирован'}, status=HTTPStatus.OK)
+
 
 
 class RequestPhoneNumberView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        phone_number = request.data.get('phone_number')
-        if not phone_number:
-            return Response({'error': 'Номер телефона обязателен'},
-                            status=HTTPStatus.BAD_REQUEST)
-        # генерация 4-значного кода
-        auth_code = str(random.randint(1000, 9999))
-        global auth_codes
-        auth_codes[phone_number] = auth_code
-        print(f"Сгенерированный код для {phone_number}: {auth_code}")
-        logger.info(f"Сгенерирован код для номера {phone_number}: {auth_code}")
+        serializer = PhoneNumberSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        phone_number = serializer.validated_data['phone_number']
 
-        # задержка отправки
-        time.sleep(2)
-        return Response({'message': f'Код отправлен на {phone_number}'},
-                        status=HTTPStatus.OK)
+        # Генерация 4-значного кода
+        auth_code = str(random.randint(1000, 9999))
+        auth_codes[phone_number] = auth_code
+
+        logger.info(f"Сгенерирован код для {phone_number}: {auth_code}")
+
+        time.sleep(2)  # Задержка для имитации отправки кода
+        return Response({'message': f'Код отправлен на {phone_number}'}, status=HTTPStatus.OK)
+
 
 
 class RequestAuthCodeView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        phone_number = request.data.get('phone_number')
-        auth_code = request.data.get('auth_code')
-
-        if not phone_number or not auth_code:
-            return Response({'error': 'Номер телефона и код обязательны'},
-                            status=HTTPStatus.BAD_REQUEST)
+        serializer = AuthCodeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        phone_number = serializer.validated_data['phone_number']
+        auth_code = serializer.validated_data['auth_code']
 
         save_code = auth_codes.get(phone_number)
         print(f"Сохраненный код для {phone_number}: {save_code}, Полученный код: {auth_code}")
@@ -100,6 +83,8 @@ class RequestAuthCodeView(APIView):
             user = User.objects.filter(phone_number=phone_number).first()
             if not user:
                 user = User.objects.create_user(phone_number=phone_number)
+                token, _ = Token.objects.get_or_create(user=user)
+                print(f"Пользователь: {user}, Токен: {token}")
             return Response({'message': f'Пользователь {phone_number} авторизован'},
                             status=HTTPStatus.OK)
         return Response({'error': 'Неверный код'},
